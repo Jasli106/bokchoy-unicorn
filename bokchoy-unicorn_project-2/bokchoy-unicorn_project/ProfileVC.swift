5b9e9b4451ea1368ceb88eb930dc1b11459647db
Jasmine <jasmineli106@gmail.com>
2019-07-17 13:28:56 -0400

    Unzipped file

---
+++ bokchoy-unicorn_project-2/bokchoy-unicorn_project/ProfileVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileVC.swift
+//  bokchoy-unicorn_project
+//
+//  Created by Jasmine Li on 6/27/19.
+//  Copyright © 2019 Jasmine Li. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
