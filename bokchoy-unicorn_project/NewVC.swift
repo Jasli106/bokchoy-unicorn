22d4c4664510494d4e57dfbcf521b7582ef20596
Cyanalkal <37746016+Cyanalkal@users.noreply.github.com>
2019-07-20 09:11:59 -0400

    NewVC 'POST' button adds an event w/ some details to database. Database reading code in TableVC not yet functional (commented partially)

--- bokchoy-unicorn_project/NewVC.swift
+++ bokchoy-unicorn_project/NewVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class NewVC: UIViewController {
     @IBOutlet weak var postButton: UIButton!
@@ -27,14 +28,42 @@ class NewVC: UIViewController {
         //let eventTitleUserInput = "exampletitlelol"
         //UserDefaults.standard.set(eventTitleUserInput, forKey: "title")
         
+        /*
+        //gives who the post author is (useful later)
+        let userID = Auth.auth().currentUser?.uid
+        ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let username = value?["username"] as? String ?? ""
+            let user = User(username: username)
+            
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        */
         
-        struct Variables {
-            static var newEvent = [
-                //"title" : eventTitleUserInput,
-                "title" : "new title!!",
-                "time" : "the best time",
-                "text" : "cats allowed"
-            ]
+        
+        let newEvent = [
+            //"title" : eventTitleUserInput,
+            "title" : "new title!!",
+            "time" : "the best time",
+            "text" : "cats allowed"
+        ]
+        
+        
+        let eventRef = Database.database().reference()
+        
+        let eventIDNumber = "101"
+        
+        let eventID = "events/event"+eventIDNumber
+        eventRef.child(eventID).setValue(newEvent){
+            (error:Error?, ref:DatabaseReference) in
+            if let error = error {
+                print("Data could not be saved: \(error).")
+            } else {
+                print("Data saved successfully!")
+            }
         }
         
         
